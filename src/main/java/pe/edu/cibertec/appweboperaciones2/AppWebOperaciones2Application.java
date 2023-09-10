1878a1e22161ce674300f02c71701bf800d4878f
santizafer <santizafer@gmail.com>
2023-09-10 14:15:41 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appweboperaciones2/AppWebOperaciones2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appweboperaciones2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebOperaciones2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebOperaciones2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appweboperaciones2/AppWebOperaciones2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appweboperaciones2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebOperaciones2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
